a9cc161a91ac0923c5275ad238a8e306a6176456
luexp <10457192@qq.com>
2024-05-14 15:39:19 +0800

    impl week1 day3

--- mini-lsm-starter/src/block/builder.rs
+++ mini-lsm-starter/src/block/builder.rs
@@ -20,22 +20,59 @@ pub struct BlockBuilder {
 impl BlockBuilder {
     /// Creates a new block builder.
     pub fn new(block_size: usize) -> Self {
-        unimplemented!()
+        Self {
+            offsets: Vec::new(),
+            data: Vec::new(),
+            block_size,
+            first_key: KeyVec::new(),
+        }
     }
 
     /// Adds a key-value pair to the block. Returns false when the block is full.
     #[must_use]
     pub fn add(&mut self, key: KeySlice, value: &[u8]) -> bool {
-        unimplemented!()
+        if self.first_key.is_empty() {
+            self.add_kv(&key, value);
+            self.first_key.set_from_slice(key);
+            return true;
+        }
+
+        let size = 8 + key.len() + value.len();
+        if size + self.data.len() + self.offsets.len() > self.block_size {
+            return false;
+        }
+
+        self.add_kv(&key, value);
+        true
+    }
+    fn add_kv(&mut self, key: &KeySlice, value: &[u8]) {
+        self.offsets.push(self.data.len() as u16);
+        self.data
+            .extend(Self::write_little_endian(key.len() as u16));
+        self.data.extend(key.raw_ref());
+        self.data
+            .extend(Self::write_little_endian(value.len() as u16));
+        self.data.extend(value);
+    }
+    pub fn write_little_endian(data: u16) -> [u8; 2] {
+        let low = (data & 0xff) as u8;
+        let high = ((data >> 8) & 0xff) as u8;
+        [low, high]
+    }
+    pub fn read_little_endian(data: &[u8]) -> u16 {
+        (data[0]) as u16 | ((data[1] as u16) << 8)
     }
 
     /// Check if there is no key-value pair in the block.
     pub fn is_empty(&self) -> bool {
-        unimplemented!()
+        self.data.is_empty()
     }
 
     /// Finalize the block.
     pub fn build(self) -> Block {
-        unimplemented!()
+        Block {
+            data: self.data.clone(),
+            offsets: self.offsets.clone(),
+        }
     }
 }
--- mini-lsm-starter/src/block/iterator.rs
+++ mini-lsm-starter/src/block/iterator.rs
@@ -5,7 +5,7 @@ use std::sync::Arc;
 
 use crate::key::{KeySlice, KeyVec};
 
-use super::Block;
+use super::{Block, BlockBuilder};
 
 /// Iterates on a block.
 pub struct BlockIterator {
@@ -34,44 +34,98 @@ impl BlockIterator {
 
     /// Creates a block iterator and seek to the first entry.
     pub fn create_and_seek_to_first(block: Arc<Block>) -> Self {
-        unimplemented!()
+        let mut itr = BlockIterator::new(block);
+        itr.seek_to_first();
+        itr
     }
 
     /// Creates a block iterator and seek to the first key that >= `key`.
     pub fn create_and_seek_to_key(block: Arc<Block>, key: KeySlice) -> Self {
-        unimplemented!()
+        let mut itr = BlockIterator::new(block);
+        itr.seek_to_first();
+        itr.seek_to_key(key);
+        itr
     }
 
     /// Returns the key of the current entry.
     pub fn key(&self) -> KeySlice {
-        unimplemented!()
+        self.key.as_key_slice()
     }
 
     /// Returns the value of the current entry.
     pub fn value(&self) -> &[u8] {
-        unimplemented!()
+        &self.block.data[self.value_range.0..self.value_range.1]
     }
 
     /// Returns true if the iterator is valid.
     /// Note: You may want to make use of `key`
     pub fn is_valid(&self) -> bool {
-        unimplemented!()
+        self.key.is_empty()
     }
 
     /// Seeks to the first key in the block.
     pub fn seek_to_first(&mut self) {
-        unimplemented!()
+        let key_len = BlockBuilder::read_little_endian(&self.block.data[0..2]) as usize;
+        let key = KeyVec::from_vec(Vec::from(&self.block.data[2..2 + key_len]));
+        let value_len =
+            BlockBuilder::read_little_endian(&self.block.data[2 + key_len..4 + key_len]) as usize;
+
+        self.first_key = key.clone();
+        self.key = key;
+        self.idx = 0;
+        self.value_range = (4 + key_len, 4 + key_len + value_len);
     }
 
     /// Move to the next key in the block.
     pub fn next(&mut self) {
-        unimplemented!()
+        self.idx = self.idx + 1;
+        if self.idx >= self.block.offsets.len() {
+            self.key = KeyVec::new();
+            self.value_range = (0, 0);
+            return;
+        }
+
+        let key_start = self.block.offsets[self.idx] as usize;
+        let key_len =
+            BlockBuilder::read_little_endian(&self.block.data[key_start..key_start + 2]) as usize;
+        let key = KeyVec::from_vec(Vec::from(
+            &self.block.data[key_start + 2..key_start + 2 + key_len],
+        ));
+        let value_start = key_start + 2 + key_len;
+        let value_len =
+            BlockBuilder::read_little_endian(&self.block.data[value_start..value_start + 2])
+                as usize;
+
+        self.key = key;
+        self.value_range = (value_start + 2, value_start + 2 + value_len);
     }
 
     /// Seek to the first key that >= `key`.
     /// Note: You should assume the key-value pairs in the block are sorted when being added by
     /// callers.
     pub fn seek_to_key(&mut self, key: KeySlice) {
-        unimplemented!()
+        for n in 0..self.block.offsets.len() {
+            let key_start = self.block.offsets[n] as usize;
+            let key_len =
+                BlockBuilder::read_little_endian(&self.block.data[key_start..key_start + 2])
+                    as usize;
+            let t_key = KeyVec::from_vec(Vec::from(
+                &self.block.data[key_start + 2..key_start + 2 + key_len],
+            ));
+
+            if t_key.as_key_slice() >= key {
+                let value_start = key_start + 2 + key_len;
+                let value_len = BlockBuilder::read_little_endian(
+                    &self.block.data[value_start..value_start + 2],
+                ) as usize;
+                self.key = t_key;
+                self.idx = n;
+                self.value_range = (value_start + 2, value_start + 2 + value_len);
+                return;
+            }
+        }
+
+        self.key = KeyVec::new();
+        self.value_range = (0, 0);
     }
 }
